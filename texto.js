89fd255b8671d869a246393eb7c8566e2ec76911
Chessie <elizabeth_nwogo@hotmail.com>
2024-10-18 23:42:27 -0300

    Cards
    Antes das alterações novas nos cards

--- texto.js
+++ texto.js
@@ -2,15 +2,36 @@
 
 const cardsProject = document.querySelectorAll(".project-card");
 
+function closeAllCardsExcept(tagCard){
+    cardsProject.forEach((card) => {
+        if(card != tagCard){
+            //console.log("remove? ", tagCard);
+            card.classList.remove("-opened");
+        }
+    });
+}
 
-const toggleHideInfo = (e) =>{
-    console.log(e.currentTarget)
+
+const toggleOpenCard = (e) =>{
     infoCard = e.currentTarget.querySelector(".project-info");
-    console.log(infoCard);
-    e.currentTarget.classList.toggle("-opened");
+    closeAllCardsExcept(e.currentTarget);
+
+    e.currentTarget.classList.add("-opened");
 };
 
 cardsProject.forEach((card) => {
-    console.log(card);
-    card.addEventListener("click", toggleHideInfo);
+    card.addEventListener("click", toggleOpenCard);
 });
+
+
+const btnCloseCard = document.querySelectorAll(".close-card");
+
+function closeCard(e){
+    e.stopPropagation(); // Evita a propagação do evento para o card
+    let cardProject = e.currentTarget.closest('.project-card')
+    cardProject.classList.remove("-opened");
+}
+btnCloseCard.forEach((btn) => {    
+    btn.addEventListener("click", closeCard);
+});
+
