7a64ca8e01e84ae0db519483895dccd74acbab0e
Chessie <elizabeth_nwogo@hotmail.com>
2024-12-05 01:01:31 -0300

    Atualizando

--- changeTheme.js
+++ changeTheme.js
@@ -1,22 +1,85 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    const svgImage = document.getElementById("svgBannerImage");
-
-    svgImage.addEventListener("load", function () {
-        const svgFile = svgImage.contentDocument; // Acessa o documento SVG
-        const lightLuminaria = svgFile.getElementById("lightLuminaria");
-        const luminaria = svgFile.getElementById("luminaria");
-
-        if (luminaria && lightLuminaria) {
-            luminaria.style.cursor = "pointer";
-            luminaria.addEventListener("click", function () {
-                // Alterna a propriedade opacity entre 0 e 1
-                if (lightLuminaria.style.opacity === "0") {
-                    lightLuminaria.style.opacity = "0.45"; // Torna visível
-                } else {
-                    lightLuminaria.style.opacity = "0"; // Torna transparente
-                }
-            });
+document.addEventListener("DOMContentLoaded", function() {
+    sliderBtn = document.querySelector(".slider");
+console.log("o btn aqui: ",sliderBtn)
+    //Mudança de tema
+    const btnSwitchTheme = document.getElementById("chk-switchTheme");
+    let lightIs = false;
+    
+    btnSwitchTheme.addEventListener('click', function(event){        
+        updateLightEffect();
+        
+        if(btnSwitchTheme.checked){
+            console.log("Escuro");
+        }else{
+            console.log("Claro");
         }
     });
+
+    function updateLightEffect(){ 
+        
+        applyTheme(); 
+        console.log(btnSwitchTheme.checked);
+        if(btnSwitchTheme.checked){
+            console.log("Modo Escuro ativado");
+            lightLuminaria.style.opacity = "0.45";
+
+            sliderBtn.querySelector('.bi').classList.remove('bi-brightness-high-fill');
+            sliderBtn.querySelector('.bi').classList.add('bi-moon-fill');
+        }else{
+            console.log("Modo Claro ativado");
+            lightLuminaria.style.opacity = "0";
+            sliderBtn.querySelector('.bi').classList.remove('bi-moon-fill');
+            sliderBtn.querySelector('.bi').classList.add('bi-brightness-high-fill');
+        }
+    }
+
+
+    /////////////////////////
+    const body = document.body;
+    function applyTheme() {
+        if (btnSwitchTheme.checked) {
+            body.classList.add("darkTheme");
+            body.classList.remove("lightTheme");
+            console.log("darkTheme");
+        } else {
+            body.classList.add("lightTheme");
+            body.classList.remove("darkTheme");
+            console.log("lightTheme");
+        }
+    }
+
+    // Aplicar o tema ao carregar a página
+    applyTheme();
+    /////////////////////////
+
+
+    //Manipulando o SVG da Luminaria
+    const svgContainer = document.querySelector('.svg-container');
+    
+    fetch('assets/images/num%20sei/teste2.svg')
+    .then(response => response.text())
+    .then(svgCode => {
+        svgContainer.innerHTML = svgCode;
+
+        // Agora o SVG foi carregado e inserido, podemos manipular seus elementos
+        const svgImage = svgContainer.querySelector('svg');
+        if (svgImage) {
+            /////console.log("SVG carregado");
+            const lightLuminaria = svgImage.getElementById("lightLuminaria");
+            const luminaria = svgImage.getElementById("luminaria");
+
+            if (luminaria && lightLuminaria) {
+                /////console.log("Elementos encontrados");
+                luminaria.style.cursor = "pointer";
+                
+                luminaria.addEventListener("click", function () {
+                    btnSwitchTheme.checked = !btnSwitchTheme.checked;
+                    updateLightEffect();
+                });
+            } else {
+                console.error("Elementos SVG não encontrados");
+            }
+        }
+    })
+    .catch(error => console.error('Erro ao carregar o SVG:', error));
 });
